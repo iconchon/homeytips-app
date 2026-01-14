e68ba1609568f48e6863fb9cf5198478f4d1c258
iconchon <bayu.wicaksono777@gmail.com>
2026-01-14 13:44:07 +0700

    test link image

--- src/App.jsx
+++ src/App.jsx
@@ -451,7 +451,7 @@ const ProductCard = ({ product, onBuy }) => {
         {/* Render Gambar: Cek jika ada property image dan belum error */}
         {product.image && !imgError ? (
           <img 
-            src={`./images/${product.image}`} 
+            src={product.image} 
             alt={product.title} 
             className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
             onError={(e) => {
